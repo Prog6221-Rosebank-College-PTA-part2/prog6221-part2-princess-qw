4f4b6c9b41e0423f8d5dd3b8325aaf816193aecd
Sellwane Khampepe <ST10485259@rcconnect.edu.za>
2026-05-29 21:34:50 +0200

    Add author information and project header

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+// sellwane khampepe st10485259 - Prog6221 Part 2 POE Submission
 
 namespace PrincessCyberGuide
 {
